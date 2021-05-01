5358ba36a5c75338ceeff5e4d804828c2f49ebc3
umutplbyk96 <padostudio@hotmail.com>
2021-05-01 09:40:36 +0300

    first commit and header component

---
+++ src/components/Header.js
@@ -0,0 +1,103 @@
+import "./Header.scss";
+import { FaTwitter } from "react-icons/fa";
+import { RiHome7Fill } from "react-icons/ri";
+import { AiOutlineNumber } from "react-icons/ai";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { RiFileListLine } from "react-icons/ri";
+import { HiOutlineUser } from "react-icons/hi";
+import { BiEnvelope } from "react-icons/bi";
+import { CgMoreO } from "react-icons/cg";
+import { FiBookmark } from "react-icons/fi";
+
+const Banner = () => {
+
+  return (
+    <div className="banner">
+      <div className="banner__wrapper">
+        <div className="banner__navigation">
+          <FaTwitter className="banner__logo" size={30} color="#fff" />
+          <ul className="navbar__items">
+            <li className="navbar__item">
+              <a href="/" className="navbar__item-link">
+                <div className="navbar__icon">
+                  <RiHome7Fill className="navbar__icon-comp" size={30} />
+                </div>
+                <div className="navbar__span">Anasayfa</div>
+              </a>
+            </li>
+            <li className="navbar__item">
+              <a href="/" className="navbar__item-link">
+                <div className="navbar__icon">
+                  <AiOutlineNumber className="navbar__icon-comp" size={30} />
+                </div>
+                <div className="navbar__span">Keşfet</div>
+              </a>
+            </li>
+            <li className="navbar__item">
+              <a href="/" className="navbar__item-link">
+                <div className="navbar__icon">
+                  <IoMdNotificationsOutline
+                    className="navbar__icon-comp"
+                    size={30}
+                  />
+                </div>
+                <div className="navbar__span">Bildirimler</div>
+              </a>
+            </li>
+            <li className="navbar__item">
+              <a href="/" className="navbar__item-link">
+                <div className="navbar__icon">
+                  <BiEnvelope className="navbar__icon-comp" size={30} />
+                </div>
+                <div className="navbar__span">Mesajlar</div>
+              </a>
+            </li>
+            <li className="navbar__item">
+              <a href="/" className="navbar__item-link">
+                <div className="navbar__icon">
+                  <FiBookmark className="navbar__icon-comp" size={30} />
+                </div>
+                <div className="navbar__span">Yer İşaretleri</div>
+              </a>
+            </li>
+            <li className="navbar__item">
+              <a href="/" className="navbar__item-link">
+                <div className="navbar__icon">
+                  <RiFileListLine className="navbar__icon-comp" size={30} />
+                </div>
+                <div className="navbar__span">Listeler</div>
+              </a>
+            </li>
+            <li className="navbar__item">
+              <a href="/" className="navbar__item-link">
+                <div className="navbar__icon">
+                  <HiOutlineUser className="navbar__icon-comp" size={30} />
+                </div>
+                <div className="navbar__span">Profil</div>
+              </a>
+            </li>
+            <li className="navbar__item">
+              <a href="/" className="navbar__item-link">
+                <div className="navbar__icon">
+                  <CgMoreO className="navbar__icon-comp" size={30} />
+                </div>
+                <div className="navbar__span">Daha Fazla</div>
+              </a>
+            </li>
+            <li className="navbar__item navbar__button">
+              <a href="/" className="navbar__tweet-button">
+                Tweetle
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="navbar__profile">
+            <div className="nav__profile-left"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
